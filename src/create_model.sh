53e6d1b145520e3d2d848f1fee12f42b5998363f
jessicam1 <67648381+jessicam1@users.noreply.github.com>
2024-05-04 21:45:45 -0400

    Update create_model.sh
    updated path

--- src/create_model.sh
+++ src/create_model.sh
@@ -3,7 +3,7 @@ source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/
 source myconda
 conda activate focalloss
 
-python create_model.py
+python src/create_model.py
 
 ### how to run
 # sbatch --cpus-per-task=4 --mem=100g --mail-type=BEGIN,END --time=5:00 kerastuner.sh
