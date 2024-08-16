7e2f17cd903ebc8f83b5965dce6b2a85d471de56
jessicam1 <67648381+jessicam1@users.noreply.github.com>
2024-08-16 10:26:28 -0400

    Delete src/create_model.sh

--- src/create_model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/profile.d/mamba.sh
-source myconda
-conda activate focalloss
-
-python src/create_model.py
-
-### how to run
-# sbatch --cpus-per-task=4 --mem=100g --mail-type=BEGIN,END --time=5:00 kerastuner.sh
