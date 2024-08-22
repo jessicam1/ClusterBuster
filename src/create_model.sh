c055c1ce65eb061750b20bc0bec356613d44a491
Jessica Martin <67648381+jessicam1@users.noreply.github.com>
2024-08-22 14:14:28 -0400

    Update create_model.sh
    Let user specify conda source and environment.

--- src/create_model.sh
+++ src/create_model.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-source /data/$USER/conda/etc/profile.d/conda.sh && source /data/$USER/conda/etc/profile.d/mamba.sh
-source myconda
-conda activate focalloss
 
-SAVEDIR=$1
-MODEL=$2
-PNAME=$3
-TRAIN=$4
-VAL=$5
+CONDA_PATH=$1
+MAMBA_PATH=$2
+ENV_NAME=$3
+
+source ${CONDA_PATH} 
+source ${MAMBA_PATH}
+conda activate ${ENV_NAME}
+
+SAVEDIR=$4
+MODEL=$5
+PNAME=$6
+TRAIN=$7
+VAL=$8
 
 python src/create_model.py --save_directory ${SAVEDIR} --model_name ${MODEL} --project_name ${PNAME} --training_data ${TRAIN} --validation_data ${VAL} --save_information
 
